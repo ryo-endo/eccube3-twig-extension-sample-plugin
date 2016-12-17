d6a1ab9e57deb3c8a97f055203e5c45222283fa4
ryo-endo <ryo_endo@lockon.co.jp>
2016-12-17 04:20:18 +0000

    add TwigExtensionSample_getMessage function.

--- ServiceProvider/TwigExtensionSampleServiceProvider.php
+++ ServiceProvider/TwigExtensionSampleServiceProvider.php
@@ -8,6 +8,13 @@ class TwigExtensionSampleServiceProvider implements ServiceProviderInterface
 {
     public function register(BaseApplication $app)
     {
+        $app['twig'] = $app->share($app->extend('twig', function (\Twig_Environment $twig, \Silex\Application $app) {
+            $twig->addFunction(new \Twig_SimpleFunction('TwigExtensionSample_getMessage', function () use ($app) {
+                return 'Hello.';
+            }));
+
+            return $twig;
+        }));
     }
 
     public function boot(BaseApplication $app)

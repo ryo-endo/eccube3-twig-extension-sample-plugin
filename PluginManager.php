f9dcf58645ea39d9c72e920008c7a8e8fcc87347
ryo-endo <ryo_endo@lockon.co.jp>
2016-12-17 04:19:28 +0000

    base plugin

---
+++ PluginManager.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * EC-CUBE 3.0 TwigExtensionSample plugin
+ *
+ * @license UNLICENSE http://unlicense.org/UNLICENSE
+ */
+
+namespace Plugin\TwigExtensionSample;
+
+use Eccube\Application;
+use Eccube\Plugin\AbstractPluginManager;
+
+class PluginManager extends AbstractPluginManager
+{
+    public function install($config, Application $app)
+    {
+    }
+
+    public function uninstall($config, Application $app)
+    {
+    }
+
+    public function enable($config, Application $app)
+    {
+    }
+
+    public function disable($config, Application $app)
+    {
+    }
+
+    public function update($config, Application $app)
+    {
+    }
+}
---
+++ ServiceProvider/TwigExtensionSampleServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Plugin\TwigExtensionSample\ServiceProvider;
+
+use Silex\Application as BaseApplication;
+use Silex\ServiceProviderInterface;
+
+class TwigExtensionSampleServiceProvider implements ServiceProviderInterface
+{
+    public function register(BaseApplication $app)
+    {
+    }
+
+    public function boot(BaseApplication $app)
+    {
+    }
+
+}
